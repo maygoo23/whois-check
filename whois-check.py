1082f5bcd94b15f74aae00eefacd8b52b7c3d4ad
maygoo23 <57462161+maygoo23@users.noreply.github.com>
2024-06-25 10:27:13 -0400

    Update whois-check.py

--- whois-check.py
+++ whois-check.py
@@ -12,16 +12,20 @@ WEBHOOK_URL = os.environ.get('WEBHOOK_URL', '')  # Get webhook URL from environm
 # Last expiration date checked
 last_expiry_date = None
 
+def log_message(message):
+    timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    print(f"[{timestamp}] {message}")
+
 def check_domain():
     global last_expiry_date
 
     if DOMAIN:
         try:
-            print(f"Checking WHOIS information for domain: {DOMAIN}")
+            log_message(f"Checking WHOIS information for domain: {DOMAIN}")
             domain_info = whois.whois(DOMAIN)
             expiry_date = domain_info.expiration_date
 
-            print(f"WHOIS information retrieved: {domain_info}")
+            log_message(f"WHOIS information retrieved: {domain_info}")
 
             if isinstance(expiry_date, list):
                 expiry_date = expiry_date[0]
@@ -39,23 +43,21 @@ def check_domain():
                     send_alert(f"Domain {DOMAIN} is expiring soon! Expiry date: {expiry_date}")
 
             else:
-                print(f"Could not find expiry date for domain {DOMAIN}")
+                log_message(f"Could not find expiry date for domain {DOMAIN}")
 
         except Exception as e:
-            print(f"Error checking domain {DOMAIN}: {e}")
+            log_message(f"Error checking domain {DOMAIN}: {e}")
     else:
-        print("No domain specified. Please provide a domain.")
-
+        log_message("No domain specified. Please provide a domain.")
 
 def send_alert(message):
     if WEBHOOK_URL:
-        print(f"Sending alert: {message}")
+        log_message(f"Sending alert: {message}")
         webhook = DiscordWebhook(url=WEBHOOK_URL, content=message)
         response = webhook.execute()
-        print(f"Alert sent. Response status: {response.status_code}")
+        log_message(f"Alert sent. Response status: {response.status_code}")
     else:
-        print("No webhook URL specified. Please provide a webhook URL.")
-
+        log_message("No webhook URL specified. Please provide a webhook URL.")
 
 if __name__ == "__main__":
     # Announce that the script is starting up
